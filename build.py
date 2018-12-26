8d8744ba04387c31b3f5df9ea60b07129e47b1f2
Derek <physci@gmail.com>
2018-12-26 13:18:02 -0500

    adds decorator for template filters.  adds more filters for struct.

--- build.py
+++ build.py
@@ -1,7 +1,6 @@
 from jinja2 import Environment, FileSystemLoader #, select_autoescape
 from subprocess import Popen, PIPE
 
-
 env = Environment(
     loader=FileSystemLoader('templates'),
     # autoescape=select_autoescape(['html', 'xml'])
@@ -13,28 +12,103 @@ lineTable = {
     "struct proc": 2337,
     "entry": 1044,
     "main": 1217,
+    "kvmalloc": 1840,
+    "setupkvm": 1818,
+    "mappages": 1760,
+    "walkpgdir": 1735,
+    "kinit1": 3131,
+    "kinit2": 3139,
+    "freerange": 3151,
+    "kfree": 3164,
+    "kalloc": 3187,
+    "sbrk": 3801,
+    "exec": 6610,
+}
+
+structTable = {
+    "run": 3115,
+    "spin lock": (3119, 3123)
+}
+
+figrefTable = {
+    "newkernelstack": "1-4",
+    "x86_pagetable": "2-1",
+    "xv6_layout": "2-2",
+    "processlayout": "2-3",
+}
+
+sheetTable = {
+    "mmu.h": 700,
+    "memlayout.h": 200,
+}
+
+addrTable = {
+    "KERNBASE": 207,
+    "PHYSTOP": 203,
+    "0xFE000000": 204,
+}
+
+macTable = {
+    "PGROUNDUP": 798,
 }
 
 
+def register(f):
+    ''' register each filter function with template environment.
+    '''
+    env.filters[f.__name__] = f
+    return f
+
+@register
 def goto_line(ident):
     #{{"fork" | goto_line}}
     temp = '<code><a href="javascript:gotoLine(%d);">%s</a></code>'
     return temp % (lineTable[ident], ident)
 
+@register
 def goto_lines(ident, start, end):
     #{{"fork" | goto_line }}
     temp = '<code><a href="javascript:gotoLines(%d, %d);">%s</a></code>'
     return temp % (start, end, ident)
 
-
+@register
 def section(title):
     #{{"exercies" | section}}
     return '<h3>§ %s</h3>' % title
 
+@register
 def chapter(title):
     #{{"exercies" | chapter }}
     return '<h1>%s</h1>' % title
 
+@register
+def addr(addrName):
+    # {{"KERNBASE" | addr}}
+    temp = '<code><a href="javascript:gotoLine(%d);">%s</a></code>'
+    return temp % (addrTable[addrName], addrName)
+
+@register
+def macro(name):
+    # {{"MACNAME" | addr}}
+    temp = '<code><a href="javascript:gotoLine(%d);">%s</a></code>'
+    return temp % (macTable[name], name)
+
+
+@register
+def sheet(name):
+    temp = '<code><a href="javascript:gotoLine(%d);">%s</a></code>'
+    return temp % (sheetTable[name], name)
+
+@register
+def figref(name):
+    temp='''
+    <a class="popoverOption" href="#fig%s" data-content="<img src='figs/fig%s.jpg' alt='figure %s'>"
+    rel="popover" data-placement="bottom" data-original-title="Figure %s">Figure %s</a>.
+    '''
+    fignum = figrefTable[name]
+    return temp % (fignum, fignum, fignum, fignum, fignum)
+
+@register
 def code(listing):
     # {{ ''' foo() \n bar() ''' | code }}
     # format this with external
@@ -44,25 +118,37 @@ def code(listing):
     if proc.returncode != 0:
         raise Exception("build.py fails to call clang-format for listing: " + listing)
 
-
     from pygments import highlight
     from pygments.lexers import CLexer
     from pygments.formatters import HtmlFormatter
 
     fmtd = highlight(result[0].decode(), CLexer(), HtmlFormatter())
     return "%s\n" % fmtd
-    
-    
-    
-    
-    return listing
 
+@register
 def line(lineNum):
     return '<a href="javascript:gotoLine(%d);">(%d)</a>' % (lineNum, lineNum)
 
+@register
+def struct(structName):
+    line = structTable[structName]
+    if type(line) == tuple:
+        return '<code><a href="javascript:gotoLines(%d, %d);">%s</a></code>' % (line + (structName,))
+    else:
+        return '<code><a href="javascript:gotoLine(%d);">%s</a></code>' % (line, structName)
+
+@register
 def appendix(name):
     return '<a href="this will be a link">Appendix %s</a>' % name
 
+@register
+def chapref(chapname):
+    return {
+        "LOCK": "4"
+    }[chapname]
+
+
+@register
 def figure(caption, num):
     temp = '''
     <br/>
@@ -77,20 +163,7 @@ def figure(caption, num):
     '''
     return temp % (num, num, num, caption)
 
-
 def main():
-    env.filters["goto_line"] = goto_line
-    env.filters["goto_lines"] = goto_lines
-    env.filters["section"] = section
-    env.filters["chapter"] = chapter
-    env.filters["code"] = code
-    env.filters["line"] = line
-    env.filters["appendix"] = appendix
-    env.filters["figure"] = figure
-    
-
-    
-    #template = env.get_template("test.html")
     template = env.get_template("index.html")
     print (template.render())    
 
